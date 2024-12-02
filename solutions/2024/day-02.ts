ac238f94122ebde36926ff7852b0effd627de045
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2024-12-02 12:22:21 +0100

    feat(2024): solve day02/part2

--- solutions/2024/day-02.ts
+++ solutions/2024/day-02.ts
@@ -1,33 +1,59 @@
 import { readLines } from "../../helpers/read-lines.ts";
 
-const isSafeReport = (report: number[]) => {
-  if (report[0] === report[1]) {
-    return false;
-  }
+const getReports = (): number[][] => {
+  const lines = readLines("02", "2024", "actual");
+  const reports = lines.map((line) =>
+    line.split(" ").map((str) => parseInt(str))
+  );
+  return reports;
+};
+
+const isSafeReport = (
+  report: number[],
+  safeCheckFn?: (report: number[], index: number) => boolean
+) => {
   const direction = report[1] - report[0] > 0 ? 1 : -1;
-  for (let i = 1; i < report.length; i += 1) {
+  for (let i = 0; i < report.length - 1; i += 1) {
     const cur = report[i];
-    const prev = report[i - 1];
-    const diff = (cur - prev) * direction;
+    const next = report[i + 1];
+    const diff = (next - cur) * direction;
     if (diff > 3 || diff < 1) {
-      return false;
+      return safeCheckFn?.(report, i) ?? false;
     }
   }
   return true;
 };
 
-const solvePart1 = () => {
-  const lines = readLines("02", "2024", "actual");
-  const reports = lines.map((line) =>
-    line.split(" ").map((str) => parseInt(str))
-  );
+const isToleratedSafeReport = (report: number[]) => {
+  return isSafeReport(report, (item, index) => {
+    return (
+      isSafeReport([...report.slice(0, index), ...report.slice(index + 1)]) ||
+      isSafeReport([...report.slice(0, index + 1), ...report.slice(index + 2)]) ||
+      isSafeReport([...report.slice(0, index - 1), ...report.slice(index)])
+    );
+  })
+};
+
+const solve = (solveFn: (report: number[]) => boolean): number => {
+  const reports = getReports();
   const sum = reports.reduce((acc, cur) => {
-    if (isSafeReport(cur)) {
+    if (solveFn(cur)) {
       return acc + 1;
     }
     return acc;
   }, 0);
+  return sum;
+}
+
+const solvePart1 = () => {
+  const sum = solve(isSafeReport);
+  console.log(sum);
+};
+
+const solvePart2 = () => {
+  const sum = solve(isToleratedSafeReport);
   console.log(sum);
 };
 
 solvePart1();
+solvePart2();
