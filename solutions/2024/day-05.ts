543bd9f8cddbda2dbde4e36f1af719740d939851
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2024-12-05 08:56:49 +0100

    feat(2024): solve day05/part1

---
+++ solutions/2024/day-05.ts
@@ -0,0 +1,69 @@
+import { solve } from "../../helpers/solve.ts";
+
+const parseInput = (
+  lines: string[]
+): [Record<string, number[]>, Record<string, number[]>, number[][]] => {
+  const splitIndex = lines.findIndex((line) => line === "");
+  const rulesForward = lines
+    .slice(0, splitIndex)
+    .map((line) => line.split("|").map((str) => parseInt(str)))
+    .reduce((acc, cur) => {
+      const [first, second] = cur;
+      if (acc[first]) {
+        acc[first].push(second);
+      } else {
+        acc[first] = [second];
+      }
+      return acc;
+    }, {} as Record<string, number[]>);
+  const rulesBackward = lines
+    .slice(0, splitIndex)
+    .map((line) => line.split("|").map((str) => parseInt(str)))
+    .reduce((acc, cur) => {
+      const [first, second] = cur;
+      if (acc[second]) {
+        acc[second].push(first);
+      } else {
+        acc[second] = [first];
+      }
+      return acc;
+    }, {} as Record<string, number[]>);
+  const updates = lines
+    .slice(splitIndex + 1)
+    .map((line) => line.split(",").map((str) => parseInt(str)));
+  return [rulesForward, rulesBackward, updates];
+};
+
+const solvePart1 = solve("05", "2024", "actual", (lines) => {
+  const [rulesForward, rulesBackward, updates] = parseInput(lines);
+
+  const sum = updates
+    .filter((update) => {
+      const visited: Record<string, boolean> = {};
+      const set = update.reduce((acc, cur) => {
+        acc[cur] = true;
+        return acc;
+      }, {});
+      return update.every((cur) => {
+        const forward = rulesForward[cur] ?? [];
+        const backward = rulesBackward[cur] ?? [];
+        if (forward.some((f) => set[f] && visited[f])) {
+          return false;
+        }
+        if (backward.some((b) => set[b] && !visited[b])) {
+          return false;
+        }
+        visited[cur] = true;
+        return true;
+      });
+    })
+    .map((update) => {
+      const mid = Math.floor(update.length / 2);
+      return update[mid];
+    })
+    .reduce((acc, cur) => acc + cur, 0);
+
+  return sum;
+});
+
+solvePart1();
