cc4d9143792a2e0bdaadb279c633d2726badd32f
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2024-12-06 09:48:17 +0100

    feat(2024): solve day06/part2

--- solutions/2024/day06.ts
+++ solutions/2024/day06.ts
@@ -25,12 +25,16 @@ type Visited = Record<`${number}-${number}`, number[]>;
 const getVisitedPositions = (
   startPosition: Position,
   matrix: string[][]
-): Visited => {
+): { status: 0 | 1; visited: Visited } => {
   const visited: Visited = {};
 
   let [i, j] = startPosition;
   let directionIndex = 0;
   while (true) {
+    if (visited[`${i}-${j}`]?.includes(directionIndex)) {
+      return { status: 1, visited };
+    }
+
     if (visited[`${i}-${j}`]) {
       visited[`${i}-${j}`].push(directionIndex);
     } else {
@@ -55,7 +59,7 @@ const getVisitedPositions = (
     }
   }
 
-  return visited;
+  return { status: 0, visited };
 };
 
 const solvePart1 = solve("06", "2024", "actual", (lines) => {
@@ -68,10 +72,40 @@ const solvePart1 = solve("06", "2024", "actual", (lines) => {
       }
     }
   }
-  const visited = getVisitedPositions(startPosition, matrix);
+  const { visited } = getVisitedPositions(startPosition, matrix);
   const count = Object.keys(visited).length;
 
   return count;
 });
 
+const solvePart2 = solve("06", "2024", "actual", (lines) => {
+  const matrix = lines.map((line) => line.split(""));
+  let startPosition: Position = [-1, -1];
+  for (let i = 0; i < matrix.length && startPosition[0] === -1; i += 1) {
+    for (let j = 0; j < matrix[0].length && startPosition[0] === -1; j += 1) {
+      if (matrix[i][j] === "^") {
+        startPosition = [i, j];
+      }
+    }
+  }
+  const { visited } = getVisitedPositions(startPosition, matrix);
+  const positions = Object.keys(visited).map((key) =>
+    key.split("-").map((str) => parseInt(str))
+  ) as Position[];
+
+  let count = 0;
+  for (let i = 0; i < positions.length; i += 1) {
+    const p = positions[i];
+    matrix[p[0]][p[1]] = "#";
+    const { status } = getVisitedPositions(startPosition, matrix);
+    if (status !== 0) {
+      count += 1;
+    }
+    matrix[p[0]][p[1]] = ".";
+  }
+
+  return count;
+});
+
 solvePart1();
+solvePart2();
