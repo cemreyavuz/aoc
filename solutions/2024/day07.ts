fce4e880aa49db8a1cb336e5ba498e208c3fd996
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2024-12-07 09:40:08 +0100

    feat(2024): solve day07/part2

--- solutions/2024/day07.ts
+++ solutions/2024/day07.ts
@@ -34,7 +34,31 @@ const isPossible = (
   );
 };
 
-const solvePart1 = solve("07", "2024", "actual", (lines) => {
+const isPossibleExtended = (
+  target: number,
+  nums: number[],
+  current: number
+): boolean => {
+  if (current === 0) {
+    return isPossibleExtended(target, nums.slice(1), nums[0]);
+  }
+
+  if (current > target) {
+    return false;
+  }
+
+  if (nums.length === 0) {
+    return target === current;
+  }
+
+  return (
+    isPossibleExtended(target, nums.slice(1), current + nums[0]) ||
+    isPossibleExtended(target, nums.slice(1), current * nums[0]) ||
+    isPossibleExtended(target, nums.slice(1), parseInt(`${current}${nums[0]}`))
+  );
+};
+
+const solvePart1 = solve("07", "2024", "example", (lines) => {
   const parsed = parseInput(lines);
   const sum = parsed.reduce((acc, [target, nums]) => {
     const possible = isPossible(target, nums, 0);
@@ -44,4 +68,15 @@ const solvePart1 = solve("07", "2024", "actual", (lines) => {
   return sum;
 });
 
+const solvePart2 = solve("07", "2024", "actual", (lines) => {
+  const parsed = parseInput(lines);
+  const sum = parsed.reduce((acc, [target, nums]) => {
+    const possible = isPossibleExtended(target, nums, 0);
+    return possible ? acc + target : acc;
+  }, 0);
+
+  return sum;
+});
+
 solvePart1();
+solvePart2();
