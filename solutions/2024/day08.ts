5bd267fb3adff9ef04ea92adf135f4e89efedd4f
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2024-12-08 09:43:55 +0100

    feat(2024): solve day08/part2

--- solutions/2024/day08.ts
+++ solutions/2024/day08.ts
@@ -18,6 +18,27 @@ const getAntinodesBasic = (
   return isValidPosition(nextPos, map) ? [nextPos] : [];
 };
 
+const getAntinodesExtended = (
+  s: Position,
+  t: Position,
+  map: string[][]
+): Position[] => {
+  const positions: Position[] = [];
+  let i = 0;
+  while (true) {
+    const nextI = t[0] * (i + 1) - s[0] * i;
+    const nextJ = t[1] * (i + 1) - s[1] * i;
+    const nextPos: Position = [nextI, nextJ];
+    if (isValidPosition(nextPos, map)) {
+      positions.push(nextPos);
+      i += 1;
+    } else {
+      break;
+    }
+  }
+  return positions;
+};
+
 const getAntennas = (map: string[][]): Record<string, Position[]> => {
   return map.reduce((acc, cur, i) => {
     cur.forEach((char, j) => {
@@ -59,4 +80,13 @@ const solvePart1 = solve("08", "2024", "actual", (lines) => {
   return Object.keys(antinodes).length;
 });
 
+const solvePart2 = solve("08", "2024", "actual", (lines) => {
+  const map = lines.map((line) => line.split(""));
+  const antennas = getAntennas(map);
+  const antinodes = getAntinodes(map, antennas, getAntinodesExtended);
+
+  return Object.keys(antinodes).length;
+});
+
 solvePart1();
+solvePart2();
