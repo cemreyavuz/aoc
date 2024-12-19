90f0c68dabfe5b628050c42ea855c2cc0b62c855
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2024-12-19 15:23:41 +0100

    feat(2024): solve day19/part2

--- solutions/2024/day19.ts
+++ solutions/2024/day19.ts
@@ -12,20 +12,22 @@ const solvePart1 = solve("19", "2024", "actual", (lines) => {
   const isPossible = (design: string) => {
     if (dp[design] !== undefined) {
       return dp[design];
-    } 
-    
+    }
+
     let result = false;
     if (design.length === 0) {
       result = true;
     } else {
-      result = Array(design.length).fill(0).some((_, index) => {
-        const left = design.slice(0, index + 1);
-        const right = design.slice(index + 1);
-        if (patterns[left]) {
-          return isPossible(right);
-        }
-        return false;
-      });
+      result = Array(design.length)
+        .fill(0)
+        .some((_, index) => {
+          const left = design.slice(0, index + 1);
+          const right = design.slice(index + 1);
+          if (patterns[left]) {
+            return isPossible(right);
+          }
+          return false;
+        });
     }
     dp[design] = result;
     return result;
@@ -39,4 +41,46 @@ const solvePart1 = solve("19", "2024", "actual", (lines) => {
   return sum;
 });
 
+const solvePart2 = solve("19", "2024", "actual", (lines) => {
+  const patterns = lines[0].split(", ").reduce((acc, cur) => {
+    acc[cur] = true;
+    return acc;
+  }, {});
+
+  const designs = lines.slice(2);
+  const dp: Record<string, number> = {};
+
+  const getPossibleCount = (design: string) => {
+    if (dp[design] !== undefined) {
+      return dp[design];
+    }
+
+    let result = 0;
+    if (design.length === 0) {
+      result = 1;
+    } else {
+      result = Array(design.length)
+        .fill(0)
+        .reduce((acc, _, index) => {
+          const left = design.slice(0, index + 1);
+          const right = design.slice(index + 1);
+          if (patterns[left]) {
+            return acc + getPossibleCount(right);
+          }
+          return acc;
+        }, 0);
+    }
+    dp[design] = result;
+    return result;
+  };
+
+  const sum = designs.reduce((acc, design) => {
+    const count = getPossibleCount(design);
+    return acc + count;
+  }, 0);
+
+  return sum;
+});
+
 solvePart1();
+solvePart2();
