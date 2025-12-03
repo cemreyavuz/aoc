81f83d9a9a4140135d5410bcd97b5fe351dbb6b0
Cüneyt Emre Yavuz <cuneyt.yavuz@cognite.com>
2025-12-03 09:09:11 +0100

    feat(2025): solve day03/part2

--- solutions/2025/day03.go
+++ solutions/2025/day03.go
@@ -3,10 +3,18 @@ package main
 import (
 	"aoc/helpers"
 	"fmt"
+	"math"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+
 	SolvePart1()
+	SolvePart2()
+
+	duration := time.Since(start)
+	fmt.Println("Execution time:", duration)
 }
 
 func SolvePart1() {
@@ -44,4 +52,42 @@ func SolvePart1() {
 	}
 
 	fmt.Println("Result:", result)
+}
+
+func SolvePart2() {
+	fmt.Println("Solving Part 2")
+
+	lines, err := helpers.ReadLines("03", "2025", helpers.Actual)
+	if err != nil {
+		fmt.Println("Error reading lines:", err)
+		return
+	}
+	fmt.Println("Read", len(lines), "lines")
+
+	result := float64(0)
+
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+
+		num := float64(0)
+		nextStartIndex := 0
+		for d := 0; d < 12; d++ {
+			curBiggestDigit := float64(-1)
+			curBiggestDigitIndex := -1
+			for j := nextStartIndex; j < len(line)-12+d+1; j++ {
+				curDigit := float64(line[j] - '0')
+				if curDigit > curBiggestDigit {
+					curBiggestDigit = curDigit
+					curBiggestDigitIndex = j
+				}
+			}
+
+			nextStartIndex = curBiggestDigitIndex + 1
+			num += curBiggestDigit * math.Pow10(12-d-1)
+		}
+
+		result += num
+	}
+
+	fmt.Printf("Result: %.0f\n", result)
 }
