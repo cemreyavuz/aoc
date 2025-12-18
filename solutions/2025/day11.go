c8a2c94c89c8a5bcb58b3eafc6a2070e48b73ae1
Cüneyt Emre Yavuz <cuneytemreyavuz@gmail.com>
2025-12-18 12:02:20 +0100

    feat(2025): solve day11/part2

--- solutions/2025/day11.go
+++ solutions/2025/day11.go
@@ -49,3 +49,65 @@ func SolveDay11Part1() {
 
 	fmt.Println("Result:", result)
 }
+
+func SolveDay11Part2() {
+	fmt.Println("Solving Part 2")
+
+	lines, err := helpers.ReadLines("11", "2025", helpers.Actual)
+	if err != nil {
+		fmt.Println("Error reading lines:", err)
+		return
+	}
+	fmt.Println("Read", len(lines), "lines")
+
+	g := make(map[string]map[string]bool)
+
+	for _, line := range lines {
+		split := strings.Split(line, ": ")
+		origin := split[0]
+		target := strings.Split(split[1], " ")
+
+		targets := make(map[string]bool)
+		for _, t := range target {
+			targets[t] = true
+		}
+		g[origin] = targets
+	}
+
+	dp := make(map[string]map[string]int)
+
+	var dfs func(node string, target string) int
+	dfs = func(node string, target string) int {
+		if _, exists := dp[node][target]; exists {
+			return dp[node][target]
+		}
+
+		if node == target {
+			return 1
+		}
+
+		count := 0
+
+		for neighbor := range g[node] {
+			count += dfs(neighbor, target)
+		}
+
+		if _, exists := dp[node]; !exists {
+			dp[node] = make(map[string]int)
+		}
+		dp[node][target] = count
+
+		return count
+	}
+
+	svrToFft := dfs("svr", "fft")
+	svrToDac := dfs("svr", "dac")
+	dacToFft := dfs("dac", "fft")
+	fftToDac := dfs("fft", "dac")
+	dacToOut := dfs("dac", "out")
+	fftToOut := dfs("fft", "out")
+
+	result := svrToDac*dacToFft*fftToOut + svrToFft*fftToDac*dacToOut
+
+	fmt.Println("Result:", result)
+}
